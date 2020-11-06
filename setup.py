3663ab2de029fac362104d6dfbe11ac5788c46ce
morban <mathieu.orban@openedition.org>
2020-11-06 16:45:02 +0100

    Autoload package data version stable

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bilbo2',
-    version = '1.1',
+    version = '1.1.0',
     packages = find_packages(),
     author = "OpenEdition Lab",
     author_email = "mathieu.orban@openedition.org",
